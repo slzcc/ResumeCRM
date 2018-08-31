61da176c7694ef353cc5dd9d666925d2c1407edd
slzcc <383821949@qq.com>
2018-08-31 09:38:44 +0800

    edit Makefile

--- Docker/docker-entrypoint.sh
+++ Docker/docker-entrypoint.sh
@@ -8,8 +8,9 @@ cat <<EOT > /etc/supervisord.conf
 nodaemon=true
 
 [program:resume]
+autorestart = false
+autostart = true
 command=/bin/bash -c "python3 manage.py runserver 0.0.0.0:8088"
-autorestart=true
 startsecs=0
 stderr_logfile=/dev/stderr
 stderr_logfile_maxbytes=0
@@ -17,8 +18,9 @@ stdout_logfile=/dev/stdout
 stdout_logfile_maxbytes=0
 
 [program:celery-worker]
+autorestart = false
+autostart = true
 command=/bin/bash -c "celery -A ResumeCRM worker -l debug --concurrency=1"
-autorestart=true
 startsecs=0
 stderr_logfile=/dev/stderr
 stderr_logfile_maxbytes=0
@@ -26,8 +28,9 @@ stdout_logfile=/dev/stdout
 stdout_logfile_maxbytes=0
 
 [program:celery-beat]
+autorestart = false
+autostart = true
 command=/bin/bash -c "celery -A ResumeCRM beat -l info -S django"
-autorestart=true
 startsecs=0
 stderr_logfile=/dev/stderr
 stderr_logfile_maxbytes=0
